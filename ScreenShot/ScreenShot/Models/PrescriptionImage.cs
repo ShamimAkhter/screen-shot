89897efc9cc4d3b56691db334b4b2fe4800f197f
Shamim Akhter <shamimakhter25@outlook.com>
2022-08-31 23:59:20 +0530

    Structure changed by adding the asp.net core web api project also

---
+++ ScreenShot/ScreenShot/Models/PrescriptionImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScreenShot.Models
+{
+  public class PrescriptionImage
+  {
+    public int Id { get; set; }
+    public string DoctorName { get; set; }
+    public byte[] ImageFile { get; set; }
+  }
+
+  public class PrescriptionImageDto
+  {
+    public int Id { get; set; }
+    public string DoctorName { get; set; }
+    public IFormFile ImageFile { get; set; }
+  }
+}
